6e6e28ea246f130f3db142794120df80e7e78f02
Michael Kleinsasser <mkleinsa@umich.edu>
2021-08-10 13:44:17 -0400

    small edits to pass CRAN requirements

---
+++ R/data.R
@@ -0,0 +1,61 @@
+
+#' Population of US states
+#'
+#' Data frame with the populations of each state.
+#'
+#' @name USA_state_N
+#' @docType data
+#' @keywords data
+#' @format a list with
+#' \itemize{
+#' \item{Province_State}{ State as a character string}
+#' \item{N}{ Population of the state}
+#' }
+#' @example data(USA_state_N)
+NULL
+
+#' Confirmed COVID-19 cases
+#'
+#' Confirmed COVID-19 cases in US states
+#'
+#' @name confirmed
+#' @docType data
+#' @keywords data
+#' @format a list with
+#' \itemize{
+#' \item{Province_State}{ name of the US state}
+#' \item{date ...}{ a column for each date}
+#' }
+#' @example data(confirmed)
+NULL
+
+#' Confirmed COVID-19 deaths
+#'
+#' Confirmed COVID-19 deaths in US states
+#'
+#' @name death
+#' @docType data
+#' @keywords data
+#' @format a list with
+#' \itemize{
+#' \item{Province_State}{ name of the US state}
+#' \item{date ...}{ a column for each date}
+#' }
+#' @example data(death)
+NULL
+
+
+#' Confirmed COVID-19 recovered
+#'
+#' Confirmed COVID-19 recovered in US states
+#'
+#' @name recovered
+#' @docType data
+#' @keywords data
+#' @format a list with
+#' \itemize{
+#' \item{Province_State}{ name of the US state}
+#' \item{date ...}{ a column for each date}
+#' }
+#' @example data(recovered)
+NULL
---
+++ R/globalVariables.R
@@ -0,0 +1,3 @@
+
+globalVariables(c("dead", "dead_med", "id", "phase", "value", "x", "xmax",
+                  "xmin", "y", "ymax", "ymin"))
